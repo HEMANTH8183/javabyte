61e5c57fbae826c7fc8b5a7e97b037f0a6488d11
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-20 21:14:04 +0530

    gitbyte

---
+++ Hemanthbit.java
@@ -0,0 +1,18 @@
+class Hemanth {
+	public static void main (String []args){
+	byte []friends = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	System.out.println(friends[1]);	
+	System.out.println(friends[0]);
+	System.out.println(friends[2]);
+	System.out.println(friends[3]);
+	System.out.println(friends[4]);
+	System.out.println(friends[5]);
+	System.out.println(friends[6]);
+	System.out.println(friends[7]);
+	System.out.println(friends[8]);
+	System.out.println(friends[9]);
+	System.out.println(friends[10]);
+
+	}
+}
+	
